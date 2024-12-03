d87abbbd8b59bfc594be8043ade2b7466781f3cc
Mykha3004 <2642phammy@gmail.com>
2024-12-03 21:15:33 +0700

    initial commit

---
+++ 11/server 1/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.x0gs0.mongodb.net',
+    DB_USER: 'ngocmy',
+    DB_PASS: '123',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'mykha3004@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'jfrecjqcdmjvqxkr',
+    JWT_SECRET: 'myyyy',
+    JWT_EXPIRES: '72000000000000', // in milliseconds
+  };
+  module.exports = MyConstants;
+  
